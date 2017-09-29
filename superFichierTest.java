a3e2cbeca4f500693f1df5e40471d37ca9557c4f
Lambin Abigail <abigail.lambin@etudiant.univ-lille1.fr>
2017-09-29 16:29:33 +0200

    modif .travis

--- superFichierTest.java
+++ superFichierTest.java
@@ -1,8 +1,6 @@
-public class HelloWorld {
-
-    public static void main(String[] args) {
-        // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, World");
-    }
-
+/** Votre premier programme Java */
+class FirstApp {
+   public static void main (String[] args){
+    System.out.println("Hello World");
+   }
 }
